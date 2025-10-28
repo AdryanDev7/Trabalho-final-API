841168b029e739190f76a60d49c1e364c689ec0b
fariiasgabriel <fariias_gabriel@hotmail.com>
2025-10-28 15:20:17 -0300

    correcao erro service

--- src/main/java/br/com/serratec/service/ClienteService.java
+++ src/main/java/br/com/serratec/service/ClienteService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.serratec.dto.ClienteResponseDTO;
 import br.com.serratec.dto.ClienteUpdateDTO;
@@ -13,6 +14,7 @@ import br.com.serratec.exception.ClienteException;
 import br.com.serratec.repository.ClienteRepository;
 import jakarta.transaction.Transactional;
 
+@Service
 public class ClienteService {
 
 	@Autowired
