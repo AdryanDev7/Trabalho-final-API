cc2d0ea6125568fcb78d59f74b5fe14f4c8b8d71
fariiasgabriel <fariias_gabriel@hotmail.com>
2025-10-29 16:01:13 -0300

    atualizacao

--- src/main/java/br/com/serratec/dto/PedidoResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package br.com.serratec.dto;
-
-import java.time.LocalDate;
-import java.util.List;
-import br.com.serratec.entity.ItemPedido;
-
-public record PedidoResponseDTO(Long id, Double valorTotal,LocalDate dataPedido, List<ItemPedido> itens) { 
-
-}
